0267f54ba5d663fff948c072203c2b2878eec75f
Jeanine Mars <jeaninemars1@gmail.com>
2017-01-26 09:22:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var firstname = 'Jeanine';
+var lastname = 'Mars';
+var name = firstname + ' ' + lastname;
+var yearsOfStudy = 1;
+var time = 'year';
+var combine = yearsOfStudy + ' ' + time;
+var goal = 'work in Web Design';
